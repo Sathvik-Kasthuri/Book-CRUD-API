3a26b51c7ea89758c0610c1e39a762f35c6253db
Sathvik Kasthuri <kasthurisathvik17@gmail.com>
2026-02-02 20:16:38 +0530

    Prevent Crash on DB Failure for render

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connection Established");
+    console.log("MONGO_URI:", process.env.MONGO_URI);
   } catch (error) {
     console.error("Connection Failed:", error.message);
-    process.exit(1);
   }
 };
 
